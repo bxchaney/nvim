745b64e4486bf70cee6b0a223730fb5d634b62ee
bxchaney <bxchaney@gmail.com>
2023-09-23 13:46:49 -0400

    reformatted, added linter

--- lua/bxchaney/packer.lua
+++ lua/bxchaney/packer.lua
@@ -1,56 +1,58 @@
 -- This file can be loaded by calling `lua require('plugins')` from your init.vim
 
 -- Only required if you have packer configured as `opt`
-vim.cmd [[packadd packer.nvim]]
+vim.cmd([[packadd packer.nvim]])
 
-return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+return require("packer").startup(function(use)
+	-- Packer can manage itself
+	use("wbthomason/packer.nvim")
 
-  use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.2',
-	  -- or                            , branch = '0.1.x',
-	  requires = { {'nvim-lua/plenary.nvim'} }
-  }
+	use({
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.2",
+		-- or                            , branch = '0.1.x',
+		requires = { { "nvim-lua/plenary.nvim" } },
+	})
 
-  use ({
-	  'Mofiqul/dracula.nvim',
-	  as = 'dracula',
-	  config = function()
-		  vim.cmd('colorscheme dracula')
-	  end
-  })
+	use({
+		"Mofiqul/dracula.nvim",
+		as = "dracula",
+		config = function()
+			vim.cmd("colorscheme dracula")
+		end,
+	})
 
-  use ('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate'})
+	use("nvim-treesitter/nvim-treesitter", { run = ":TSUpdate" })
 
-  use ('nvim-treesitter/playground')
-  
-  use ('ThePrimeagen/harpoon')
+	use("nvim-treesitter/playground")
 
-  use ('mbbill/undotree') 
-  use ('tpope/vim-fugitive') 
-  use ('APZelos/blamer.nvim')
-  use {
-	  'VonHeikemen/lsp-zero.nvim',
-	  branch = 'v2.x',
-	  requires = {
-		  -- LSP Support
-		  {'neovim/nvim-lspconfig'},             -- Required
-		  {                                      -- Optional
-		  'williamboman/mason.nvim',
-		  run = function()
-			  pcall(vim.cmd, 'MasonUpdate')
-		  end,
-	  },
-      {'mhartington/formatter.nvim'},
-	  {'williamboman/mason-lspconfig.nvim'}, -- Optional
-	  {"williamboman/nvim-lsp-installer"},
+	use("ThePrimeagen/harpoon")
 
-	  -- Autocompletion
-	  {'hrsh7th/nvim-cmp'},     -- Required
-	  {'hrsh7th/cmp-nvim-lsp'}, -- Required
-	  {'L3MON4D3/LuaSnip'},     -- Required
-      {'rafamadriz/friendly-snippets'},
-  }
-}
+	use("mbbill/undotree")
+	use("tpope/vim-fugitive")
+	use("APZelos/blamer.nvim")
+	use({
+		"VonHeikemen/lsp-zero.nvim",
+		branch = "v2.x",
+		requires = {
+			-- LSP Support
+			{ "neovim/nvim-lspconfig" }, -- Required
+			{ -- Optional
+				"williamboman/mason.nvim",
+				run = function()
+					pcall(vim.cmd, "MasonUpdate")
+				end,
+			},
+			{ "mhartington/formatter.nvim" },
+			{ "williamboman/mason-lspconfig.nvim" }, -- Optional
+			{ "williamboman/nvim-lsp-installer" },
+
+			-- Autocompletion
+			{ "hrsh7th/nvim-cmp" }, -- Required
+			{ "hrsh7th/cmp-nvim-lsp" }, -- Required
+			{ "L3MON4D3/LuaSnip" }, -- Required
+			{ "rafamadriz/friendly-snippets" },
+		},
+	})
+	use("mfussenegger/nvim-lint")
 end)
