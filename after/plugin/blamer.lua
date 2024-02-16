8916802fe5bf48e84fc0d7548e084cfb019524c3
bxchaney <bxchaney@gmail.com>
2024-02-16 12:13:20 -0500

    added blamer

--- after/plugin/blamer.lua
+++ after/plugin/blamer.lua
@@ -1,3 +1,7 @@
-vim.g.blamer_prefix = ' > '
-vim.g.blamer_template = '<committer> | <committer-time> | <summary>'
+vim.g.blamer_prefix = " > "
+vim.g.blamer_template = "<committer> | <committer-time> | <summary>"
 vim.g.blamer_enabled = 0
+
+vim.api.nvim_create_user_command("BT", function()
+	vim.cmd("BlamerToggle")
+end, {})
